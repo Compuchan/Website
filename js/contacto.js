a9fef550f136f93319554a60d41e5b07bbbe7279
j3r3s <jerito97@hotmail.com.ar>
2020-10-01 13:44:42 -0300

    validacion dinamica v0.4 estilos dinamicos, eventlistener (submit, ochange inputs), border bottom, error messages,

--- js/contacto.js
+++ js/contacto.js
@@ -1,74 +1,153 @@
-function regexValidator(e) {
+//Inputs
+const names = document.querySelector("#name-input");
+const enterprise = document.querySelector("#enterp-input");
+const email = document.querySelector("#email-input");
+const tel = document.querySelector("#phone-input");
+const message = document.querySelector("#description-input");
 
-const name = document.querySelector("#name-input").value;
-const enterprise = document.querySelector("#enterp-input").value;
-const tel = document.querySelector("#phone-input").value;
-const email = document.querySelector("#email-input").value;
-const message = document.querySelector("#description-input").value;
+//Sumbmit
+const SubmitButton = document.querySelector("#submit-btn")
 
-// Mensajes de error
+// Error messages form
 const nameMsg = document.querySelector(".error-name");
 const enterpriseMsg = document.querySelector(".error-empresa");
-const telMsg = document.querySelector(".error-tel");
 const emailMsg = document.querySelector(".error-email");
+const telMsg = document.querySelector(".error-tel");
+const messageMsg = document.querySelector(".error-message");
 
 // RegEx
-const regName = /[A-Za-z]{1}[A-Za-z]/;
-const regEnterprise = /[A-Za-z]{1}[A-Za-z]/;
-const regTel = /[0-9]{8}/;
-const regEmail = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+const regexName = /[A-Za-z]{1}[A-Za-z]/;
+const regexEnterprise = /[A-Za-z]{1}[A-Za-z]/;
+const regexEmail = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+const regexTel = /[0-9]{8}/;
 
 
-// Validacion RegExp
 
 
-	if (regName.test(name) == false) {
+// EventListeners
+names.addEventListener("change", validOnWrite);
+enterprise.addEventListener("change", validOnWrite);
+email.addEventListener("change", validOnWrite);
+tel.addEventListener("change", validOnWrite);
+message.addEventListener("change", validOnWrite);
+
+SubmitButton.addEventListener("click", checkSubmit);
+
+
+
+
+function validOnWrite() {
+
+	if (regexName.test(names.value) == false && names.value != "") {
 
 		nameMsg.classList.add("error-msg");
+		names.classList.add("error-input")
+	} else {
+
+		nameMsg.classList.remove("error-msg" );
+		names.classList.remove("error-input")
 	}
 
-	if (regEnterprise.test(enterprise) == false) {
+	if (regexEnterprise.test(enterprise.value) == false && enterprise.value != "") {
+
+		enterpriseMsg.classList.add("error-msg");
+		enterprise.classList.add("error-input")
+	} else {
+
+		enterpriseMsg.classList.remove("error-msg");
+		enterprise.classList.remove("error-input")
+	}
+	
+	if (regexEmail.test(email.value) == false && email.value != "") {
+
+		emailMsg.classList.add("error-msg");
+		email.classList.add("error-input")
+	} else {
+
+		emailMsg.classList.remove("error-msg");
+		enterprise.classList.remove("error-input")
+	}
+
+	if (regexTel.test(tel.value) == false && tel.value != "") {
+
+		telMsg.classList.add("error-msg");
+		tel.classList.add("error-input")
+	} else {
+
+		telMsg.classList.remove("error-msg");
+		enterprise.classList.remove("error-input")
+	}
+	
+	if ( message.value != "") {
+
+		messageMsg.classList.remove("error-msg");
+		message.classList.remove("error-input")
+	}
+	
+}
+
+
+function checkSubmit() {
+
+	if (!regexName.test(names.value) || names.value == "" ||
+		!regexEnterprise.test(enterprise.value) || enterprise.value == "" ||
+		!regexEmail.test(email.value) || email.value == "" ||
+		!regexTel.test(tel.value) || tel.value == "" ||
+		message.value == "") {
+
+		validOnSubmit();
+
+	} else {
+
+		submitToAPI();
+
+	}
+
+}
+
+
+function validOnSubmit() {
+
+	if (regexName.test(names.value) == false || names.value == "") {
+
+		nameMsg.classList.add("error-msg");
+	} 
+
+	if (regexEnterprise.test(enterprise.value) == false || enterprise.value == "") {
 
 		enterpriseMsg.classList.add("error-msg");
 	}
+	
+	if (regexEmail.test(email.value) == false || email.value == "") {
 
-	if (regTel.test(tel) == false) {
+		emailMsg.classList.add("error-msg");
+	}
+
+	if (regexTel.test(tel.value) == false || tel.value == "") {
 
 		telMsg.classList.add("error-msg");
 	}
 
-	if (regEmail.test(email) == false || email == "") {
+	if ( message.value == "") {
 
-		emailMsg.classList.add("error-msg");
+		messageMsg.classList.add("error-msg");
 	}
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-function submitToAPI(e) {
-
-	e.preventDefault();
+function submitToAPI() {
 
 	//const URL = "https://kg0114k1ek.execute-api.us-east-1.amazonaws.com/beta/contact-us";
 
 
 	let data = {
-		Nombre: name,
-		Empresa: enterprise,
-		Telefono: tel,
-		Email: email,
-		Mensaje: message
+		Nombre: names.value,
+		Empresa: enterprise.value,
+		Email: email.value,
+		Telefono: tel.value,
+		Mensaje: message.value
 	};
 
 	console.log("Data: ", data)
