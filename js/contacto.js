601529b34952cff7d54afcce97bebb0e7551416d
j3r3s <jerito97@hotmail.com.ar>
2020-09-30 22:22:55 -0300

    adding onchange events

--- js/contacto.js
+++ js/contacto.js
@@ -1,67 +1,92 @@
+function regexValidator(e) {
+
+const name = document.querySelector("#name-input").value;
+const enterprise = document.querySelector("#enterp-input").value;
+const tel = document.querySelector("#phone-input").value;
+const email = document.querySelector("#email-input").value;
+const message = document.querySelector("#description-input").value;
+
+// Mensajes de error
+const nameMsg = document.querySelector(".error-name");
+const enterpriseMsg = document.querySelector(".error-empresa");
+const telMsg = document.querySelector(".error-tel");
+const emailMsg = document.querySelector(".error-email");
+
+// RegEx
+const regName = /[A-Za-z]{1}[A-Za-z]/;
+const regEnterprise = /[A-Za-z]{1}[A-Za-z]/;
+const regTel = /[0-9]{8}/;
+const regEmail = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+
+
+// Validacion RegExp
+
+
+	if (regName.test(name) == false) {
+
+		nameMsg.classList.add("error-msg");
+	}
+
+	if (regEnterprise.test(enterprise) == false) {
+
+		enterpriseMsg.classList.add("error-msg");
+	}
+
+	if (regTel.test(tel) == false) {
+
+		telMsg.classList.add("error-msg");
+	}
+
+	if (regEmail.test(email) == false || email == "") {
+
+		emailMsg.classList.add("error-msg");
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function submitToAPI(e) {
-    e.preventDefault();
 
-    const URL = "https://kg0114k1ek.execute-api.us-east-1.amazonaws.com/beta/contact-us";
+	e.preventDefault();
 
-    const name = document.querySelector("#name-input").value;
-    const empresa = document.querySelector("#enterp-input").value;
-    const phone = document.querySelector("#phone-input").value;
-    const email = document.querySelector("#email-input").value;
-    const desc = document.querySelector("#description-input").value;
+	//const URL = "https://kg0114k1ek.execute-api.us-east-1.amazonaws.com/beta/contact-us";
 
 
-    /// VALIDA CAMPO NOMBRE
-    let Namere = /[A-Za-z]{1}[A-Za-z]/;
-    if (!Namere.test(name)) {
-        alert ("El nombre debe ser de 2 o mas caracteres");
-        return
-    }
-    /// VALIDA CAMPO EMPRESA
-    let Enterprise = /[A-Za-z]{1}[A-Za-z]/;
-    if (!Enterprise.test(empresa)) {
-        alert ("La empresa debe ser de 2 o mas caracteres");
-       return
-    }
-    /// VALIDA CAMPO TELEFONO
-    let mobilere = /[0-9]{8}/;
-    if (!mobilere.test(phone)) {
-        alert ("Ingrese un numero telefonico valido");
-       return
-    }
-    /// VALIDA CAMPO EMAIL
-        let reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
-        if (!reeamil.test(email) || email == "") {
-            alert ("Ingrese una direccion de correo valida");
-            return
-        }
-    
-    
-    if(Namere.test(name),Enterprise.test(Enterprise),mobilere.test(phone),reeamil.test(email) == true && email != true) {
+	let data = {
+		Nombre: name,
+		Empresa: enterprise,
+		Telefono: tel,
+		Email: email,
+		Mensaje: message
+	};
 
+	console.log("Data: ", data)
 
-    let data = {
-        name : name,
-        empresa: empresa,
-        phone : phone,
-        email : email,
-        desc : desc
-    };
-    console.log("Data: ", data)
+	// fetch(URL, {
+	// 	method: 'POST', // or 'PUT'
+	// 	mode: 'no-cors',
+	// 	body: JSON.stringify(data), // data can be `string` or {object}!
+	// 	headers: {
+	// 		'Content-Type': 'application/json',
+	// 		'Accept': '*/*'
+	// 	}
+	// })
 
-    fetch(URL, {
-        method: 'POST', // or 'PUT'
-        mode: 'no-cors',
-        body: JSON.stringify(data), // data can be `string` or {object}!
-        headers:{
-          'Content-Type': 'application/json',
-          'Accept': '*/*'
-        }
-    })
-    .catch(error => console.error('Error:', error))
-    .then(response => {
-        console.log('Success:', response)
-        document.getElementById("contact-form").reset();
-        alert("Su consulta fue enviada, pronto nos pondremos en contacto");
-    });
-  }
+	// 	.catch(error => console.error('Error:', error))
+	// 	.then(response => {
+	// 		console.log('Success:', response)
+	// 		document.getElementById("contact-form").reset();
+	// 		alert("Su consulta fue enviada, pronto nos pondremos en contacto");
+	// 	});
 }
