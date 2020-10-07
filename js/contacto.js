5fcb4c2e7270613a702a972f66b26a1ef0290ef7
j3r3s <jerito97@hotmail.com.ar>
2020-10-07 15:59:03 -0300

    border bottom, labels ok/ng

--- js/contacto.js
+++ js/contacto.js
@@ -22,6 +22,9 @@ const emailMsg = document.querySelector("#error-email");
 const telMsg = document.querySelector("#error-tel");
 const messageMsg = document.querySelector("#error-message");
 
+//Submit ok
+const Submitok = document.querySelector("#ok-message")
+
 // RegEx
 const regexName = /[A-Za-z]{1}[A-Za-z]/;
 const regexEnterprise = /[A-Za-z]{1}[A-Za-z]/;
@@ -32,26 +35,21 @@ const regexTel = /[0-9]{8}/;
 
 
 // EventListeners
-names.addEventListener("change", validOnWrite);
-enterprise.addEventListener("change", validOnWrite);
-email.addEventListener("change", validOnWrite);
-tel.addEventListener("change", validOnWrite);
-message.addEventListener("change", validOnWrite);
+names.addEventListener("input", validOnWrite);
+enterprise.addEventListener("input", validOnWrite);
+email.addEventListener("input", validOnWrite);
+tel.addEventListener("input", validOnWrite);
+message.addEventListener("input", validOnWrite);
+
+
+names.addEventListener("focus", validOnWrite);
+enterprise.addEventListener("focus", validOnWrite);
+email.addEventListener("focus", validOnWrite);
+tel.addEventListener("focus", validOnWrite);
+message.addEventListener("focus", validOnWrite);
+
 
 SubmitButton.addEventListener("click", checkSubmit);
-
-
-
-
-
-let inputList = [names, enterprise, email, tel, message];
-
-inputList.forEach(element => {
-	element.classList.add("error-msg")
-});
-	
-
-
 
 
 
@@ -62,40 +60,54 @@ function validOnWrite() {
 	if (regexName.test(names.value) == false && names.value != "") {
 
 		nameMsg.classList.add("error-msg");
+		names.classList.add("error-input");
 	} else {
 
 		nameMsg.classList.remove("error-msg" );
+		names.classList.remove("error-input" );
 	}
 
 	if (regexEnterprise.test(enterprise.value) == false && enterprise.value != "") {
 
 		enterpriseMsg.classList.add("error-msg");
+		enterprise.classList.add("error-input");
 	} else {
 
 		enterpriseMsg.classList.remove("error-msg");
+		enterprise.classList.remove("error-input");
 	}
 	
 	if (regexEmail.test(email.value) == false && email.value != "") {
 
 		emailMsg.classList.add("error-msg");
+		email.classList.add("error-input");
 	} else {
 
 		emailMsg.classList.remove("error-msg");
+		email.classList.remove("error-input");
 	}
 
 	if (regexTel.test(tel.value) == false && tel.value != "") {
 
 		telMsg.classList.add("error-msg");
+		tel.classList.add("error-input");
 	} else {
 
 		telMsg.classList.remove("error-msg");
+		tel.classList.remove("error-input");
 	}
 	
 	if ( message.value != "") {
 
 		messageMsg.classList.remove("error-msg");
+		message.classList.remove("error-input");
 	}
 	
+}
+
+function focusing() {
+
+		labelNames.classList.add("on-writing-label")
 }
 
 
@@ -123,26 +135,31 @@ function validOnSubmit() {
 	if (regexName.test(names.value) == false || names.value == "") {
 
 		nameMsg.classList.add("error-msg");
+		names.classList.add("error-input");
 	} 
 
 	if (regexEnterprise.test(enterprise.value) == false || enterprise.value == "") {
 
 		enterpriseMsg.classList.add("error-msg");
+		enterprise.classList.add("error-input");
 	}
 	
 	if (regexEmail.test(email.value) == false || email.value == "") {
 
 		emailMsg.classList.add("error-msg");
+		email.classList.add("error-input");
 	}
 
 	if (regexTel.test(tel.value) == false || tel.value == "") {
 
 		telMsg.classList.add("error-msg");
+		tel.classList.add("error-input");
 	}
 
 	if ( message.value == "") {
 
 		messageMsg.classList.add("error-msg");
+		message.classList.add("error-input");
 	}
 }
 
