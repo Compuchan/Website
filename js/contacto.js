cb6cae8b4aeb569fbe4c49cc3ef4d2883851fe35
j3r3s <jerito97@hotmail.com.ar>
2021-05-05 15:54:46 -0300

    redisain form 3, validation and submit OK

--- js/contacto.js
+++ js/contacto.js
@@ -1,14 +1,16 @@
 const formulario = document.getElementById('contact-form');
-const inputs = document.querySelectorAll('#contact-form input');
+const inputs = document.querySelectorAll('#contact-form input, textarea');
 
 // RegEx
 const regex = {
 
-	regNombre = /[A-Za-z]{1}[A-Za-z]/,
-	regEmpresa = /[A-Za-z]{1}[A-Za-z]/,
-	regEmail = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/,
-	regTelefono = /[0-9]{8}/
+	regNombre: /[A-Za-z]{1}[A-Za-z]/,
+	regEmpresa: /[A-Za-z]{1}[A-Za-z]/,
+	regEmail: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+	regTelefono: /[0-9]{8}/,
+	regMensaje: /^(.{2,})$/
 }
+
 
 const campos = {
 	nombre: false,
@@ -16,6 +18,52 @@ const campos = {
 	email: false,
 	telefono: false,
 	mensaje: false
+}
+
+
+
+const validForm = (e) => {
+	switch (e.target.name){
+		case "nombre":	
+			validField(regex.regNombre, e.target, 'nombre')
+		break
+		case "empresa":	
+			validField(regex.regEmpresa, e.target, 'empresa')
+		break
+		case "email":	
+			validField(regex.regEmail, e.target, 'email')
+		break
+		case "telefono":	
+			validField(regex.regTelefono, e.target, 'telefono')
+		break
+		case "mensaje":	
+			validField(regex.regMensaje, e.target, 'mensaje')
+		break
+	}
+
+}
+
+
+
+const validField = (expresion, input, field) => {
+	if (expresion.test(input.value)) {
+		
+		document.querySelector(`#grupo_${field} #${field}`).classList.remove("input-bad")
+		document.querySelector(`#grupo_${field} #${field}`).classList.add("input-ok");
+		document.querySelector(`#grupo_${field} label`).classList.remove("label-bad");
+		document.querySelector(`#grupo_${field} label`).classList.add("label-ok");
+		document.querySelector(`#grupo_${field} p`).classList.remove("error-msg-show");
+		campos[field] = true;
+
+}	else {
+		document.querySelector(`#grupo_${field} #${field}`).classList.add("input-bad")
+		document.querySelector(`#grupo_${field} #${field}`).classList.remove("input-ok")
+		document.querySelector(`#grupo_${field} label`).classList.remove("label-ok")
+		document.querySelector(`#grupo_${field} label`).classList.add("label-bad")
+		document.querySelector(`#grupo_${field} p`).classList.add("error-msg-show")
+	campos[field] = false;
+
+	}
 }
 
 
@@ -27,52 +75,31 @@ inputs.forEach((input) => {
 });
 
 
-SubmitButton.addEventListener("click", checkSubmit);
 
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
 
-
-
-
-
-
-const validForm = (e) => {
-	switch (e.target.name){
-		case "usuario":	
-			validField(regex.name, e.target, 'usuario')
-		break
-		case "usuario":	
-			validField(regex.name, e.target, 'usuario')
-		break
-		case "usuario":	
-			validField(regex.name, e.target, 'usuario')
-		break
-		case "usuario":	
-			validField(regex.name, e.target, 'usuario')
-		break
-		case "usuario":	
-			validField(regex.name, e.target, 'usuario')
-		break
-	}
-
-}
-
-
-const validField = (expresion, input, field) => {
-	if (expresion.test(input.value)) {
+	if (campos.nombre && campos.empresa && campos.email && campos.telefono && campos.mensaje) {
 		
-		document.querySelector(`${field}`).classList.remove()
+		formulario.reset()
+
+		document.getElementById('ok-message').classList.add('ok-message-show');
+		setTimeout(() => {
+			document.getElementById('ok-message').classList.remove('ok-message-show');
+		}, 5000);
+
+		document.querySelectorAll(label).classList.remove("label-ok");
+		document.querySelectorAll(input, textarea).classList.remove("input-ok");
+		
 	}
-
 }
+	
+);
 
 
 
 
-function checkSubmit() {
 
-
-
-}
 
 
 
